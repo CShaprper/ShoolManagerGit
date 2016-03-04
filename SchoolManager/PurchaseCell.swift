2167962c9f797551c71d6b99659c3037c0cc7c0d
Peter Sypek <peter.sypek@gmx.de>
2016-03-04 14:43:45 +0100

    Final

--- SchoolManager/PurchaseCell.swift
+++ SchoolManager/PurchaseCell.swift
@@ -17,9 +17,10 @@ class PurchaseCell: UITableViewCell {
     private var _ProductDescription:String?
     private var _ProductHeader:String?
     
-    func configureCell(product:SKProduct, appDel:AppDelegate){
+    func configureCell(product:SKProduct){
         self.productDescription.text! = product.localizedDescription
         self.productTitle.text! = product.localizedTitle
+        self.productPrice.backgroundColor = UIColor.blackColor()    
         self.productPrice.text! = String(product.localizedPrice())
         self.productPrice.layer.cornerRadius  = 5
     }
